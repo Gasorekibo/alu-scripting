7983ef7d52b78d5091f34472d28ef317f97ebe4a
Gasorekibo <m.gasore@alustudent.com>
2023-03-07 11:47:46 -0300

    top 10 post title

---
+++ api_advanced/1-top_ten.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Finding hot top 10 post
+"""
+import requests
+
+
+def top_ten(subreddit):
+    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+    headers = {'User-Agent' : 'Myapi-app'}
+
+    r= requests.get(url+'?limit=10', headers=headers)
+    if r.status_code == 200:
+        value = r.json()
+        datas = value['data']['children']
+        
+        for each in datas:
+            title = each['data']['title']
+            return title
+        
+    else:
+        return 0

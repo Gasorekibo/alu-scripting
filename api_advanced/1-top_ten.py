eb392768d79cf960913729593faeb52ca58cbfa3
Gasorekibo <m.gasore@alustudent.com>
2023-03-07 11:53:38 -0300

    top 10 post title

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -9,7 +9,7 @@ def top_ten(subreddit):
     url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
     headers = {'User-Agent': 'Myapi-app'}
 
-    r= requests.get(url + '?limit=10', headers=headers)
+    r = requests.get(url+'?limit=10', headers=headers)
     if r.status_code == 200:
         value = r.json()
         datas = value['data']['children']

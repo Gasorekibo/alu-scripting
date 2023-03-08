5c55329e6458ef07efeb7696b88287702edb8c9f
Gasorekibo <m.gasore@alustudent.com>
2023-03-08 06:35:17 -0300

    recursive function

---
+++ api_advanced/2-recurse.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+making a recursive function
+"""
+import requests
+
+
+def recurse(subreddit, hot_list=[]):
+    
+    def requesting(url):
+        url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+        headers = {'User-Agent': 'Myapi-app'}
+        r = requests.get(url, headers=headers)
+        if r.status_code == 200:
+            result = r.json()
+            data = result['data']['children']
+            for title in data:
+                hot_list.append(title['data']['title'])
+                return(hot_list)
+            after = result['data']['after']
+            if after is None:
+                return None
+            else:
+                return(requesting(after))
+        return None

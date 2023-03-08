0c10e5cfee69e407b6e3ee0d8d465f8f31898635
Gasorekibo <m.gasore@alustudent.com>
2023-03-08 08:26:09 -0300

    recurse it

--- api_advanced/2-recurse.py
+++ api_advanced/2-recurse.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 """
-python scrip that uses recursive function to 
-retrieve all post title and store them in 
-a list in our case it is hot_list
+recursive function
 """
 import requests
 

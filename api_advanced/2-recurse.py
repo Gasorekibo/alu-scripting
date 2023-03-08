57c56197b6c3d35c343882c2ea1a1e9ec5c9f947
Gasorekibo <m.gasore@alustudent.com>
2023-03-08 10:23:11 -0300

    All top 10 hot post title using recursive function

--- api_advanced/2-recurse.py
+++ api_advanced/2-recurse.py
@@ -10,7 +10,7 @@ def recurse(subreddit, hot_list=[], after=''):
     headers = {'User-Agent': 'Myapi-app'}
     parameter = {'after': after}
 
-    r = requests.get(subreddit_url, headers= headers,params=parameter,
+    r = requests.get(subreddit_url, headers= headers, params=parameter,
                      allow_redirects=False)
     if r.status_code == 200:
         datas = r.json()

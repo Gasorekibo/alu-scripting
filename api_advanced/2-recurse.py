33aeead98bb86e7051997a28896636a68410e984
Gasorekibo <m.gasore@alustudent.com>
2023-03-08 08:19:24 -0300

    recursive function

--- api_advanced/2-recurse.py
+++ api_advanced/2-recurse.py
@@ -5,22 +5,24 @@ making a recursive function
 import requests
 
 
-def recurse(subreddit, hot_list=[], after=None,count = 0):
-    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+def recurse(subreddit, hot_list=[], after=''):
+    subreddit_url = f'https://www.reddit.com/r/{subreddit}/hot.json'
     headers = {'User-Agent': 'Myapi-app'}
-    parameter = {'after': after}
-    r = requests.get(url+"?limit=100", headers=headers, param=parameter)
+    parameter = {'after':after}
+
+    r = requests.get(subreddit_url, headers=headers,params=parameter,
+                     allow_redirect=False)
+    
     if r.status_code == 200:
-        data = r.json()
-        results = data['data']['children']
-        for each in results:
-            hot_list.append(each['data']['title'])
-        
-        after = data['data']['after']
+        datas = r.json()
+        values = datas['data']['children']
+
+        for each in values:
+            title = each['data']['title']
+            hot_list.append(title)
+        after = datas['data']['after']
+
         if after is not None:
-            count += 1
-            return recurse(subreddit, after=after,
-                            hot_list=hot_list, count=count)
-        else:
-            return hot_list
+            return recurse(subreddit, hot_list, after)
+        return hot_list
     return None

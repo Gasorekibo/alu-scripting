b996ce07892807e6b7e9db7dd1421c1316f72a29
Gasorekibo <m.gasore@alustudent.com>
2023-03-10 08:22:02 -0300

    c

--- api_advanced/3-count.py
+++ api_advanced/3-count.py
@@ -4,50 +4,41 @@ import json
 import requests
 
 
-def count_words(subreddit, word_list, after="", count=[]):
-    """ prints a sorted count of given keywords """
-
-    if after == "":
-        count = [0] * len(word_list)
-
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    request = requests.get(url,
-                           params={'after': after},
-                           allow_redirects=False,
-                           headers={'User-Agent': 'Mozilla/5.0'})
-
-    if request.status_code == 200:
-        data = request.json()
-
-        for topic in (data['data']['children']):
-            for word in topic['data']['title'].split():
-                for i in range(len(word_list)):
-                    if word_list[i].lower() == word.lower():
-                        count[i] += 1
-
-        after = data['data']['after']
-        if after is None:
-            save = []
-            for i in range(len(word_list)):
-                for j in range(i + 1, len(word_list)):
-                    if word_list[i].lower() == word_list[j].lower():
-                        save.append(j)
-                        count[i] += count[j]
-
-            for i in range(len(word_list)):
-                for j in range(i, len(word_list)):
-                    if (count[j] > count[i] or
-                            (word_list[i] > word_list[j] and
-                             count[j] == count[i])):
-                        aux = count[i]
-                        count[i] = count[j]
-                        count[j] = aux
-                        aux = word_list[i]
-                        word_list[i] = word_list[j]
-                        word_list[j] = aux
-
-            for i in range(len(word_list)):
-                if (count[i] > 0) and i not in save:
-                    print("{}: {}".format(word_list[i].lower(), count[i]))
-        else:
-            count_words(subreddit, word_list, after, count)
+def count_words(subreddit, word_list, limit=100, after=None, count=0, posts={}):
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit={limit}"
+    if after:
+        url += f"&after={after}"
+    
+    # Set the user-agent header to avoid being blocked by the API
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    
+    # Send the API request
+    response = requests.get(url, headers=headers)
+    
+    # Check if the request was successful
+    if response.status_code == 200:
+        # Parse the JSON response
+        data = response.json()
+        posts_data = data['data']['children']
+        
+        # Process the posts
+        for post_data in posts_data:
+            title = post_data['data']['title'].lower()
+            for keyword in word_list:
+                if keyword.lower() in title and (' '+keyword.lower()+' ' in title or title.startswith(keyword.lower()+' ') or title.endswith(' '+keyword.lower())):
+                    if keyword not in posts:
+                        posts[keyword] = 1
+                    else:
+                        posts[keyword] += 1
+        
+        # Check if there are more posts to retrieve
+        if data['data']['after'] and count < limit:
+            count += len(posts_data)
+            count_words(word_list, subreddit, limit, data['data']['after'], count, posts)
+    
+    # Print the count of each keyword found
+    for keyword in sorted(posts):
+        print(f"{keyword}: {posts[keyword]}")
+    
+    # Return the count of each keyword found
+    return posts
